35264fe1b9fb5fdc802765bdc05d07d70d3ad356
Joshua Equere <marveloper.8@gmail.com>
2024-07-22 19:19:01 +0100

    add graphq modue with apollo server configuration

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -1,12 +1,17 @@
-import { ApolloDriverConfig } from "@nestjs/apollo";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql'
+import { join } from "path";
 
 @Module({
   imports: [
     NestGraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchema
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      introspection: true,
     })
   ]
 })
+export class GraphQLModule {}
